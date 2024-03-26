1bae34e1d5dcd5f876f263e8200aa049d79da79f
Hexvel <dimaabdukarimov1234@gmail.com>
2024-03-26 20:01:56 +0300

    update convex deploy key to prod

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import React from 'react'
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+const convex = new ConvexReactClient(process.env.CONVEX_DEPLOY_KEY!)
 
 export const ConvexClientProvider = ({
 	children,

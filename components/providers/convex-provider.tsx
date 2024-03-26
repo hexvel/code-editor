eee76fe0140501e020debcc803128e340e9e4916
Hexvel <dimaabdukarimov1234@gmail.com>
2024-03-26 20:44:13 +0300

    fix

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import React from 'react'
 
-const convex = new ConvexReactClient(process.env.CONVEX_DEPLOY_KEY!)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export const ConvexClientProvider = ({
 	children,
